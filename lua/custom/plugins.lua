ce549291ca1f692debc3d913ca258164c5fc8448
David Rivera <davidriverg@gmail.com>
2024-01-09 23:56:08 -0500

    Added sorround.nvim to config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -147,6 +147,31 @@ return {
 		end,
 	},
 
+	-- Parenthesis, brackets sorround.
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+
+	{
+		"roobert/surround-ui.nvim",
+		dependencies = {
+			"kylechui/nvim-surround",
+			"folke/which-key.nvim",
+		},
+		config = function()
+			require("surround-ui").setup({
+				root_key = "S",
+			})
+		end,
+	},
+
 	-- To make a plugin not be loaded
 	-- {
 	--   "NvChad/nvim-colorizer.lua",
