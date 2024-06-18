9f26e3062580947b3ac7c3e291fa56c5040dc2ed
Riverdave <davidriverg@gmail.com>
2024-06-18 16:08:29 -0400

    Update repo as of mid june 2024

--- lua/after/plugins/colors.lua
+++ lua/after/plugins/colors.lua
@@ -9,66 +9,79 @@ function ColorBg(color, plugin)
   vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
 end
 
-require("no-clown-fiesta").setup({
-  transparent = true, -- Enable this to disable the bg color
-  styles = {
-    -- You can set any of the style values specified for `:h nvim_set_hl`
-    comments = { italic = true},
-    functions = {},
-    keywords = {},
-    lsp = { underline = true },
-    match_paren = {},
-    type = {  },
-    variables = { --[[ italic = true ]]},
+-- require('github-theme').setup({
+--   options = {
+--     -- Compiled file's destination location
+--     compile_path = vim.fn.stdpath('cache') .. '/github-theme',
+--     compile_file_suffix = '_compiled', -- Compiled file suffix
+--     hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+--     hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+--     transparent = false,       -- Disable setting background
+--     terminal_colors = true,    -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+--     dim_inactive = false,      -- Non focused panes set to alternative background
+--     module_default = true,     -- Default enable value for modules
+--     styles = {                 -- Style to be applied to different syntax groups
+--       -- comments = 'NONE',       -- Value is any valid attr-list value `:help attr-list`
+--       -- functions = 'NONE',
+--       -- keywords = 'NONE',
+--       -- variables = 'NONE',
+--       -- conditionals = 'NONE',
+--       -- constants = 'NONE',
+--       -- numbers = 'NONE',
+--       -- operators = 'NONE',
+--       -- strings = 'NONE',
+--       -- types = 'NONE',
+--     },
+--     inverse = {                -- Inverse highlight for different types
+--       match_paren = false,
+--       visual = false,
+--       search = false,
+--     },
+--     darken = {                 -- Darken floating windows and sidebar-like windows
+--       floats = false,
+--       sidebars = {
+--         enabled = true,
+--         list = {},             -- Apply dark background to specific windows
+--       },
+--     },
+--     modules = {                -- List of various plugins and additional options
+--       -- ...
+--     },
+--   },
+--   palettes = {},
+--   specs = {},
+--   groups = {},
+-- })
+
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = false,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
   },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
 })
-
-require('github-theme').setup({
-  options = {
-    -- Compiled file's destination location
-    compile_path = vim.fn.stdpath('cache') .. '/github-theme',
-    compile_file_suffix = '_compiled', -- Compiled file suffix
-    hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
-    hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-    transparent = false,       -- Disable setting background
-    terminal_colors = true,    -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false,      -- Non focused panes set to alternative background
-    module_default = true,     -- Default enable value for modules
-    styles = {                 -- Style to be applied to different syntax groups
-      -- comments = 'NONE',       -- Value is any valid attr-list value `:help attr-list`
-      -- functions = 'NONE',
-      -- keywords = 'NONE',
-      -- variables = 'NONE',
-      -- conditionals = 'NONE',
-      -- constants = 'NONE',
-      -- numbers = 'NONE',
-      -- operators = 'NONE',
-      -- strings = 'NONE',
-      -- types = 'NONE',
-    },
-    inverse = {                -- Inverse highlight for different types
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-    darken = {                 -- Darken floating windows and sidebar-like windows
-      floats = false,
-      sidebars = {
-        enabled = true,
-        list = {},             -- Apply dark background to specific windows
-      },
-    },
-    modules = {                -- List of various plugins and additional options
-      -- ...
-    },
-  },
-  palettes = {},
-  specs = {},
-  groups = {},
-})
+-- vim.cmd("colorscheme gruvbox")
 
 
-local curr_scheme = "github_dark_default"
+local curr_scheme = "gruvbox"
 
 vim.cmd.colorscheme(curr_scheme) -- Or the desired theme
 -- ColorBg(_, curr_scheme)
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -415,6 +415,85 @@ return {
     config = function()
       require("nvim-ts-autotag").setup()
     end,
+  },
+
+  {
+    "slugbyte/lackluster.nvim"
+  },
+
+
+  {
+    "Mofiqul/vscode.nvim",
+
+  },
+
+  {
+    "kdheepak/monochrome.nvim"
+  },
+
+  {
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  -- Cool plugin to learn vim word navigation
+  -- {
+  --   "tris203/precognition.nvim",
+  --   config = {
+  --
+  --   }
+  -- },
+  {
+    "lunacookies/vim-colors-xcode"
+  },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require('after.plugins.colors')
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   }
+
+
+
 
 }
